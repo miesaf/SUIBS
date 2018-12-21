80cd0a807fb859501e6551c77726af2118a6bd8d
The Blue Moody <muhd.mirul95@gmail.com>
2018-12-22 04:30:36 +0800

    Fix on non-working image or logo display. Fix on app name for login template. Addition of UiTM logo. Include user manual.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-SUIBS
+**Sport Unit Inventory Borrowing System (SUIBS)**
+=====================
+
+## Note
+
+**This is a project required for `ITT544 - Web Technology` course under `CS251 - Bachelor of Netcentric Computing` program at `MARA University of Technology Malacca Branch Jasin Campus`.**
+
+## Group Particulars
+
+Group Name : `ASAS`<br/>
+Class : `M3CS2514A`<br/>
+Semester : `Sept 2018 - Jan 2019`
+
+## Group Members
+
+- Mohammad Shahzlee Bin Hasim
+- Muhamad Amirul Ikhmal Bin Azman
+- Mohammad Sharif Bin Ismail
+- Muhammad Aiman Syazani Bin Zainalabideen
+
+## User Manual
+
+User manual was included in this repository with the name [User_Manual_SUIBS.pdf](https://github.com/miesaf/SUIBS/blob/master/User_Manual_SUIBS.pdf) located at the parent directory.
+
+## Hosted Project
+
+ The project was hosted and can be accessed at  [https://app.suibs.ml](https://app.suibs.ml) for preview and evaluation.
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -58,7 +58,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/home') }}" class="brand-link">
-      <img src="./img/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="/img/uitm.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">S U I B S</span>
     </a>
@@ -68,7 +68,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="./img/project.png" class="img-circle elevation-2" alt="User Image">
+          <img src="/img/project.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">
@@ -83,7 +83,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item">
-              <a href="#" class="nav-link">
+              <a href="/home" class="nav-link">
                 <i class="nav-icon fas fa-tachometer-alt"></i>
                 <p>
                   Dashboard
@@ -188,10 +188,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-      Anything you want
+      Sport Unit Inventory Borrowing System (SUIBS)
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2018 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2018 <a href="https://app.suibs.ml">ASAS@ITT544</a>.</strong> All rights reserved.
   </footer>
 </div>
 <!-- ./wrapper -->
