36e53e3bb99c2b902244a3a1e3dfcbf056119355
The Blue Moody <muhd.mirul95@gmail.com>
2019-10-09 18:19:06 +0800

    Database configuration update

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'sql36.main-hosting.eu'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'u969337545_daryg'),
+            'username' => env('DB_USERNAME', 'u969337545_qunen'),
+            'password' => env('DB_PASSWORD', 'c1L]2h#P5@jBz:?Ixc'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
